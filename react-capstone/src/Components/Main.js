dea124d12d83acae3c06ce89adbbd2fc2affb701
Zephaniah-Willard <zepiwillard@gmail.com>
2024-05-21 21:14:59 -0500

    commiting working api's, not exactly accoring to assignment?

--- react-capstone/src/Components/Main.js
+++ react-capstone/src/Components/Main.js
@@ -1,73 +1,93 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import BookingForm from './BookingForm';
-import {fetchAPI, submitAPI} from ''
+import { fetchAPI, submitAPI } from '../Utils/SimulateApi';
 
+const updateTimes = (availableTimes, date) => {
+  const response = fetchAPI(new Date(date));
+  return response.length !== 0 ? response : availableTimes;
+};
 
-const Main = ()=>{
-  const [availableTimes, dispatchOnDateChange] = useReducer(
-    updateTimes, [], initializeTimes
-);
-//dev-api
-//API"s not defined. 
-    const fetchAPI = () =>{
-      fetch(availableTimes)
-      .then(response => response.json())
-      .then(date => dispatchOnDateChange(date))
-    };
+const initializeTimes = () => {
+  return fetchAPI(new Date());
+};
 
-    useEffect(() => {
-      fetchAPI()
-    }, []);
+const Main = () => {
+  const [availableTimes, dispatchOnDateChange] = useReducer(updateTimes, [], initializeTimes);
 
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
-    const updateTimes = (availableTimes, date) =>{
-      const response = fetchAPI(new Date(date))
-      return response.length !== 0 ? response : availableTimes
-    };
+  const handleFormSubmit = (e, formValue) => {
+    e.preventDefault();
+    const isSuccess = submitAPI(formValue);
+    setIsFormSubmitted(isSuccess);
+  };
 
-    const fetchData = () =>{
-      
-    }
-
-    const initializeTimes = initializeAvailableTimes =>{
-      const fetchData = 
-      [...initializeAvailableTimes, ...fetchAPI(new Date())]
-    };
-
-    const submitData = formData =>{
-      const response = submitAPI(formData);
-
-    }
-
-    // useEffect(() => {
-    //   if (date) {
-    //     const fetchAvailableTimes = async () => {
-    //       await new Promise((resolve) => setTimeout(resolve, 500));
-
-    //       const newTimes = [
-    //         '17:00',
-    //         '18:00',
-    //         '19:00',
-    //         '20:00',
-    //         '21:00',
-    //         '22:00',
-    //       ].sort(() => Math.random() - 0.5);
-
-    //       setAvailableTimes(newTimes);
-    //     };
-
-    //     fetchAvailableTimes();
-    //   }
-    // }, [date]);
-
-    return(
-        <main>Main
-            <BookingForm
-                availableTimes={availableTimes}
-                dispatchOnDateChange={dispatchOnDateChange}
-                />
-        </main>
-    );
+  return (
+    <main>
+      Main
+      <BookingForm
+        availableTimes={availableTimes}
+        dispatchOnDateChange={dispatchOnDateChange}
+        onFormSubmit={handleFormSubmit}
+        isFormSubmitted={isFormSubmitted}
+      />
+    </main>
+  );
 };
 
 export default Main;
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useReducer, useState } from 'react';
+// import BookingForm from './BookingForm';
+// import { fetchAPI, submitAPI } from '../Utils/SimulateApi';
+
+
+// const updateTimes = (availableTimes, date) =>{
+//   const response = fetchAPI(new Date(date))
+//   return response.length !== 0 ? response : availableTimes
+// };
+
+
+// const initializeTimes = initializeAvailableTimes =>
+//   [...initializeAvailableTimes, ...fetchAPI(new Date())];
+
+
+
+// const Main = ()=>{
+//   const [availableTimes, dispatchOnDateChange] = useReducer(
+//     updateTimes, [], initializeTimes
+// );
+
+//   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+//   const handleFormSubmit = (e) => {
+//     e.preventDefault();
+//     setIsFormSubmitted(true);
+    
+//   }
+// //dev-api
+
+//     return(
+//         <main>Main
+//             <BookingForm
+//                 availableTimes={availableTimes}
+//                 dispatchOnDateChange={dispatchOnDateChange}
+//                 onFormSubmit={handleFormSubmit}
+//                 isFormSubmitted={isFormSubmitted}
+//                 />
+//         </main>
+//     );
+// };
+
+// export default Main;
